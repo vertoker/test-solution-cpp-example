9a14f8240af21c39010ecd6fd53f74e02e9fed20
Kostya <vertogpro@gmail.com>
2025-07-01 17:24:01 +0500

    almost finish

--- TestExe/src/TestExe.cpp
+++ TestExe/src/TestExe.cpp
@@ -1,14 +1,30 @@
 // TestExe.cpp : This file contains the 'main' function. Program execution begins and ends there
 
-#include "testlib.h"
-#include "testdll.h"
-#include <fmt/base.h> // vcpkg integrate install
+#include "testlib.h" // test static library linkage
+#include "testdll.h" // test dynamic library linkage
+
+#include <fmt/base.h> // test external packages (vcpkg integrate install)
+
+#include "GLFW/glfw3.h" // test static/dynamic thirdparty library linkage
 
 int main()
 {
 	testlib::print("Test .lib");
 	print("Test .dll");
 	fmt::print("Test vcpkg\n");
+
+	int result = glfwInit();
+	if (!result)
+	{
+		fmt::print("Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	fmt::print("GLFW initialized successfully\n");
+
+	glfwTerminate();
+
+	return 0;
 }
 
 // TODO vcpkg, tests, res, lib, dll
