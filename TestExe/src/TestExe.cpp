a3326dc1ed12367e29cff04795ddfc0626d91d51
Kostya <vertogpro@gmail.com>
2025-07-01 15:08:12 +0500

    update Release configurations

--- TestExe/src/TestExe.cpp
+++ TestExe/src/TestExe.cpp
@@ -1,16 +1,14 @@
-// TestExe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// TestExe.cpp : This file contains the 'main' function. Program execution begins and ends there
 
-//#include "../../TestLib/src/framework.h"
 #include "testlib.h"
 #include "testdll.h"
-#include <fmt/base.h>
+#include <fmt/base.h> // vcpkg integrate install
 
 int main()
 {
 	testlib::print("Test .lib");
 	print("Test .dll");
-	fmt::print("Test fmt\n");
+	fmt::print("Test vcpkg\n");
 }
 
-// TODO vcpkg, tests, res
+// TODO vcpkg, tests, res, lib, dll
